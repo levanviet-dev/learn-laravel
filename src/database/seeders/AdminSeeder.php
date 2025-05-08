5e4bb899e5db794abef548c95df01b81f96ca9b3
viet.le <viet.le@kiaisoft.com.vn>
2025-05-08 15:58:23 +0700

    Initial commit

--- src/database/seeders/AdminSeeder.php
+++ src/database/seeders/AdminSeeder.php
@@ -24,5 +24,17 @@ class AdminSeeder extends Seeder
             'name' => 'Admin LTE 2',
             'role_id' => 1
         ]);
+        Admin::create([
+            'admin_code' => 'admin3',
+            'password' => 'password@123',
+            'name' => 'Admin LTE 3',
+            'role_id' => 1
+        ]);
+        Admin::create([
+            'admin_code' => 'admin4',
+            'password' => 'password@123',
+            'name' => 'Admin LTE 4',
+            'role_id' => 1
+        ]);
     }
 }
